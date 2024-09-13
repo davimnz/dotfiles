3a3ba998cc3a859478650dd83c8e4666a8418e13
davi <davi.muniz41@gmail.com>
2024-09-13 20:44:55 -0300

    feat: bash customizations

---
+++ bash/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Include .bash_customizations in .bashrc
+cp .bash_customizations ~/
+cp ~/.bashrc ~/.bashrc.bak
+cat << EOL >> ~/.bashrc
+
+# user customizations
+if [ -f ~/.bash_customizations ]; then
+    . ~/.bash_customizations
+fi
+EOL
