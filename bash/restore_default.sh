138a77de9171d0c406293cb73a7f130ff19125c6
davi <davi.muniz41@gmail.com>
2024-09-13 20:46:13 -0300

    fix: typo

--- bash/restore_default.sh
+++ bash/restore_default.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Restore to default settings for Ubuntu-based systems
+# Restore to default settings on Ubuntu-based systems
 cp ~/.bashrc ~/.bashrc.bak
 cp /etc/skel/.bashrc ~/
 source ~/.bashrc
