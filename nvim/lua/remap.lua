6277b1f31d6e99da6a2fde075b9c30e3fd39d5ea
davi <davi.muniz41@gmail.com>
2025-04-12 09:34:07 -0300

    prune: nvim config

--- nvim/lua/remap.lua
+++
@@ -1,8 +0,0 @@
--- open filesystem tree
-vim.api.nvim_set_keymap('n', '<leader>v', ':Neotree toggle<CR>', { noremap = true, silent = true })
-
--- telescope command-line sugar
-vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true })
